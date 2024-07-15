9bb9bb8c5a5addafcc1142bfa30fe73fb0e7ed85
Omkar Patil <omkarpatil290@gmail.com>
2024-07-15 15:31:25 +0530

    module-patch: add docs to v0.1.1

--- bloomfilter.go
+++ bloomfilter.go
@@ -1,3 +1,5 @@
+// Package bloomfilter is a lightweight implimentation
+// of bloomfilter algorithm in pure golang.
 package bloomfilter
 
 import (
@@ -6,13 +8,18 @@ import (
 	"math"
 )
 
+// Bloom represents a Bloom filter data structure
 type Bloom struct {
-	fp_prob    float64
-	size       uint64
-	hash_count int
-	bit_array  []bool
+	fp_prob    float64 // Desired false positive probability
+	size       uint64  // Size of the bit array
+	hash_count int     // Number of hash functions
+	bit_array  []bool  // Bit array representing the Bloom filter
 }
 
+// New creates a new Bloom filter with the specified item count and false positive probability
+// item_count: The number of items expected to be stored in the filter
+// fp_prob: The desired false positive probability
+// Returns a pointer to the Bloom filter and an error if any
 func New(item_count int, fp_prob float64) (*Bloom, error) {
 	if item_count <= 0 {
 		return nil, fmt.Errorf("item_count must be greater than 0, got %d", item_count)
@@ -44,6 +51,10 @@ func New(item_count int, fp_prob float64) (*Bloom, error) {
 	}, nil
 }
 
+// hash generates a hash value for the given item and hash index
+// item: The item to be hashed
+// i: The index of the hash function
+// Returns the computed hash value as uint64
 func (b *Bloom) hash(item string, i int) uint64 {
 	hash1 := fnv.New64()
 	hash2 := fnv.New64a()
@@ -52,6 +63,8 @@ func (b *Bloom) hash(item string, i int) uint64 {
 	return (hash1.Sum64() + uint64(i)*hash2.Sum64()) % b.size
 }
 
+// Add inserts an item into the Bloom filter
+// item: The item to be added
 func (b *Bloom) Add(item string) {
 	for i := 0; i < b.hash_count; i++ {
 		pos := b.hash(item, i)
@@ -59,6 +72,9 @@ func (b *Bloom) Add(item string) {
 	}
 }
 
+// Contains checks if an item is in the Bloom filter
+// item: The item to be checked
+// Returns true if the item is probably in the filter, false otherwise
 func (b *Bloom) Contains(item string) bool {
 	for i := 0; i < b.hash_count; i++ {
 		pos := b.hash(item, i)
